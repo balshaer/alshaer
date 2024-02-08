1a6766eac69173200bd32c77eba875cb8f3a93da
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-02-08 17:04:09 +0200

    feat: create "Projects" page.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,74 +1,80 @@
+import DescriptionOfSection from "@/components/custom/DescriptionOfSection";
+import TitleOfSection from "@/components/custom/TitleOfSection";
+import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-import projectsData, { ProjectCard } from "@/data/projectsData";
-import React, { useState, useEffect } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import ProjectsSkeleton from "@/components/skeleton/ProjectsSkeleton";
-import ProjectCardSkills from "@/components/projects/ProjectsCardSkills";
-import { t } from "i18next";
+import { Link } from "react-router-dom";
 
-const Projects: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const animate = "animate__animated animate__fadeIn";
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
-  const openWebsite = (website: string | undefined) => {
-    if (website) {
-      window.open(website, "_blank");
-    }
-  };
+export default function Projects() {
+  const projects = [
+    { id: 1, title: "Project 1" },
+    { id: 2, title: "Project 2" },
+    // Add more project data as needed
+  ];
 
   return (
-    <div className="container h-[100vh] flex flex-col gap-[1rem]   ">
-      <Navbar />
+    <ScrollArea className="h-full w-full ">
+      <div className="container h-[100vh] flex flex-col gap-[1.5rem]   ">
+        <Navbar />
 
-      <>
-        <p className="font-bold mb-4 text-[var(--headline)]">
-          {t("Projects.Projects")}
-        </p>
-        <ScrollArea className="h-[100%] w-[100%] rounded-md mb-5 ">
-          <div>
-            {projectsData.map((project: ProjectCard, index: number) => (
-              <div
-                key={index}
-                className="mb-4 bg-[var(--card-background)] py-5 px-5 "
-              >
-                {isLoading ? (
-                  <div className="card-content flex flex-col gap-3 h-full w-full">
-                    <ProjectsSkeleton />
-                  </div>
-                ) : (
-                  <div className="card-content flex flex-col gap-3 h-full w-full ">
-                    <p
-                      className={` font-semibold text-[var(--headline)] cursor-pointer ${animate}`}
-                      onClick={() => openWebsite(project.links.website)}
-                    >
-                      {project.name}
-                    </p>
-                    <div>
-                      <p
-                        className={`text-paragraph text-[var(--paragraph)] ${animate}`}
-                      >
-                        {project.description}
-                      </p>
-                    </div>
+        <div>
+          <TitleOfSection title={"Projects"} />
 
-                    <ProjectCardSkills project={project} />
+          <DescriptionOfSection description="I've worked on tons of little projects over the years but these are the ones that I'm most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas on how it can be improved." />
+
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 py-8">
+            <a href="https://naj.shamilapp.com/" target="_blank">
+              <div className="h-32 rounded-lg bg-[var(--card-background)] border-[#323a4d] hover:border-[#596788]   hovered border cursor-pointer">
+                <div className="flex p-4 gap-4 items-center justify-start h-full w-full">
+                  <div className="bg-white p-2  rounded-xl">
+                    <img
+                      src="https://naj.shamilapp.com/static/media/Logo.db28aec4cb4cdcd75f2cb1cf4be26384.svg"
+                      alt="logo"
+                      height={100}
+                      width={100}
+                    />
                   </div>
-                )}
+
+                  <div className="flex gap-1 flex-col">
+                    <span className="text-[var(--headline)]">Naj</span>
+
+                    <span className="text-[var(--paragraph)]">
+                      Dedicated center providing education to all dental
+                      students in Saudi Arabia.
+                    </span>
+                  </div>
+                </div>
               </div>
-            ))}
-          </div>
-        </ScrollArea>
-      </>
-    </div>
-  );
-};
+            </a>
+            <a href="https://github.com/balshaer/gradients-css" target="_blank">
+              <div className="h-32 rounded-lg bg-[var(--card-background)] border-[#323a4d] hover:border-[#596788]   hovered border cursor-pointer">
+                <div className="flex p-4 gap-4 items-center justify-start h-full w-full">
+                  <div className="p-2 ">
+                    <img
+                      src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/external-colors-photography-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"
+                      alt="logo"
+                      height={50}
+                      width={50}
+                    />
+                  </div>
 
-export default Projects;
+                  <div className="flex gap-1 flex-col">
+                    <span className="text-[var(--headline)]">
+                      Gradients CSS
+                    </span>
+
+                    <span className="text-[var(--paragraph)]">
+                      Collection of CSS gradients
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </ScrollArea>
+  );
+}
