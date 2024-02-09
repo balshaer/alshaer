454585629048f2560556e3fc4df98c8560a84a7c
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-02-09 08:38:45 +0200

    fix: "sideMenu & NavbarLinks"

--- src/components/navbar/NavbarLinks.tsx
+++ src/components/navbar/NavbarLinks.tsx
@@ -1,3 +1,4 @@
+// NavbarLinks.tsx
 import { Link } from "react-router-dom";
 import LanguageSelect from "../languageSelect/LanguageSelect";
 import { t } from "i18next";
@@ -10,24 +11,19 @@ interface NavItemProps {
   closeMenu: () => void;
 }
 
-export default function NavbarLinks({ closeMenu }: NavItemProps) {
+export default function NavbarLinks({ closeMenu }: { closeMenu: () => void }) {
   return (
     <ul className="flex items-center justify-center gap-4 px-4 font-[600] max-md:flex-col max-md:h-full max-md:w-full max-md:items-start max-md:text-lg max-md:space-y-1 max-md:px-4 max-md:py-6">
-      <NavItem
-        to="/"
-        className="max-md:block hidden text-[var(--nav-item)] text-md hover:text-[var(--link-color)]"
-        onClick={closeMenu}
-        closeMenu={closeMenu} // Add closeMenu prop here
-      >
+      <NavItem to="/" closeMenu={closeMenu}>
         {t("Navbar.Home")}
       </NavItem>
-      <NavItem to="/about" onClick={closeMenu} closeMenu={closeMenu}>
+      <NavItem to="/about" closeMenu={closeMenu}>
         {t("Navbar.About")}
       </NavItem>
-      <NavItem to="/projects" onClick={closeMenu} closeMenu={closeMenu}>
+      <NavItem to="/projects" closeMenu={closeMenu}>
         {t("Navbar.Projects")}
       </NavItem>
-      <NavItem to="/blog" onClick={closeMenu} closeMenu={closeMenu}>
+      <NavItem to="/blog" closeMenu={closeMenu}>
         {t("Navbar.Blog")}
       </NavItem>
       <hr className="hidden max-md:block opacity-15 w-full z-50" />
@@ -38,13 +34,12 @@ export default function NavbarLinks({ closeMenu }: NavItemProps) {
   );
 }
 
-function NavItem({ to, children, ...rest }: NavItemProps) {
+function NavItem({ to, children, closeMenu }: NavItemProps) {
   return (
-    <li
-      className="text-[var(--nav-item)] text-md hover:text-[var(--link-color)]"
-      {...rest}
-    >
-      <Link to={to}>{children}</Link>
+    <li className="text-[var(--nav-item)] text-md hover:text-[var(--link-color)]">
+      <Link to={to} onClick={closeMenu}>
+        {children}
+      </Link>
     </li>
   );
 }
