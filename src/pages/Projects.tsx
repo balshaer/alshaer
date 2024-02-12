27206208034b7fd9b2c3728ba4718ff7d1d059df
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-02-12 16:28:31 +0200

    chore: Integrate project card with badges.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,14 +7,15 @@ import DescriptionOfSection from "@/components/custom/DescriptionOfSection";
 import TitleOfSection from "@/components/custom/TitleOfSection";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-
 import NajLogo from "@/assets/images/logo/naj.jpeg";
+import { Badge } from "@/components/ui/badge";
 
 interface ProjectCardProps {
   titleKey: string;
   descriptionKey: string;
   link: string;
   logoSrc: string;
+  badgeText?: string[]; // Change to accept an array of strings
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -22,27 +23,45 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   descriptionKey,
   link,
   logoSrc,
+  badgeText,
 }) => {
   const { t } = useTranslation();
 
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
-      <div className="h-32 rounded-lg bg-[var(--card-background)] border-[#323a4d] hover:border-[#596788] hovered border cursor-pointer">
-        <div className="flex p-4 gap-4 items-center justify-start h-full w-full">
-          <div className="p-2">
-            <img
-              src={logoSrc}
-              className="min-h-10 min-w-10 rounded-lg object-contain"
-              alt="logo"
-              height={50}
-              width={50}
-            />
+      <div className="h-40 rounded-lg bg-[var(--card-background)] border-[#323a4d] hover:border-[#596788] hovered border cursor-pointer">
+        <div className="flex p-4 gap-4 items-start justify-start h-full w-full flex-col ">
+          <div className="flex items-center justify-start gap-4">
+            <div className="p-2">
+              <img
+                src={logoSrc}
+                className="min-h-10 min-w-10 rounded-lg object-contain"
+                alt="logo"
+                height={50}
+                width={50}
+              />
+            </div>
+            <div className="flex gap-1 flex-col">
+              <span className="text-[var(--headline)]">{t(titleKey)}</span>
+              <span className="text-[var(--paragraph)]">
+                {t(descriptionKey)}
+              </span>
+            </div>
           </div>
 
-          <div className="flex gap-1 flex-col">
-            <span className="text-[var(--headline)]">{t(titleKey)}</span>
-            <span className="text-[var(--paragraph)]">{t(descriptionKey)}</span>
-          </div>
+          {/* Map over badgeText array and render Badge component for each badge */}
+          {badgeText && (
+            <div className="flex gap-2">
+              {badgeText.map((badge, index) => (
+                <Badge
+                  key={index}
+                  className="m-[5px] cursor-text bg-[var(--badge-color)] text-[var(--paragraph)]"
+                >
+                  {badge}
+                </Badge>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </a>
@@ -72,13 +91,15 @@ const Projects: React.FC = () => {
                 descriptionKey="Projects.Project1.Description"
                 link="https://naj.shamilapp.com/"
                 logoSrc={NajLogo}
+                badgeText={["React JS", "Material ui"]}
               />
 
               <ProjectCard
                 titleKey="Projects.Project2.Title"
                 descriptionKey="Projects.Project2.Description"
-                link="https://github.com/balshaer/gradients-css"
+                link="https://github.com/balshaer/gradimix"
                 logoSrc="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/external-colors-photography-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"
+                badgeText={["Next JS", "Typescript"]}
               />
             </div>
           </div>
