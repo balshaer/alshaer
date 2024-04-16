8d81cc6acdd0c94fa83fbd6880c6f03ee3a63fc6
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-04-16 22:13:45 +0300

    delete 87 files

--- client/src/components/about/Contact.tsx
+++ client/src/components/about/Contact.tsx
@@ -1,40 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TitleOfSection from "../custom/TitleOfSection";
 import AnimatedComponent from "../animations/AnimatedComponent";
 import { useTranslation } from "react-i18next";
-import axios from "axios";
-
-interface SocialMediaLink {
-  title: string;
-  link: string | undefined;
-  name: string;
-  url: string;
-}
+import socialMediaLinks from "@/data/SocialMediaLink";
 
 const Contact: React.FC = () => {
   const { t } = useTranslation();
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const [fetchedLinks, setFetchedLinks] = useState<SocialMediaLink[]>([]);
 
   const handleLinkHover = (index: number | null) => {
     setHoveredIndex(index);
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:8000/api/socialLinks/show"
-        );
-        setFetchedLinks(res.data);
-
-        console.log(res.data);
-      } catch (error) {
-        console.log("Failed to fetch data with error: " + error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <section className="section" data-aos="fade-up">
@@ -42,8 +18,8 @@ const Contact: React.FC = () => {
         <TitleOfSection title={t("About.Contact.FindMe")} />
 
         <ul className="text-[var(--paragraph)] max-md:flex-col items-start hovered flex gap-4 ">
-          {fetchedLinks.map((item, index) => (
-            <li key={index}>
+          {socialMediaLinks.map((item, index) => (
+            <li key={item._id}>
               <a
                 className={`flex flex-row-reverse items-center  hovered gap-2 w-full ${
                   hoveredIndex !== null && index !== hoveredIndex
