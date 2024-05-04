f3102b49a387b77228913f4eb29983772d739f73
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-05-04 20:42:05 +0300

    update DrawerDialogDemo.tsx

--- client/src/core/components/layouts/contact/DrawerDialogDemo.tsx
+++ client/src/core/components/layouts/contact/DrawerDialogDemo.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { cn } from "@/lib/utils";
+import { Button } from "@/core/components/ui/button";
 import "animate.css";
 import { AiOutlineReload } from "react-icons/ai";
-
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -14,6 +15,8 @@ import {
 import {
   Drawer,
   DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -21,16 +24,20 @@ import {
 import { Input } from "@/core/components/ui/input";
 import { useMediaQuery } from "@react-hook/media-query";
 import { Textarea } from "@/core/components/ui/textarea";
+import { ReloadIcon } from "@radix-ui/react-icons";
 import { toast } from "sonner";
+import { HiOutlineExclamationCircle } from "react-icons/hi2";
+import { HiOutlineCheckCircle } from "react-icons/hi2";
+
 import ButtonDefault from "@/core/components/ui/ButtonDefault";
 import { HiMail } from "react-icons/hi";
 import i18n from "@/i18n";
 import { useTranslation } from "react-i18next";
 
 const styles = {
-  form: "grid items-start gap-4",
+  form: "grid items-start gap-4 ",
   inputGrid: "grid gap-2",
-  button: "text-sm focus:outline-none",
+  button: "text-sm focus:outline-none ",
   dialogContent: "sm:max-w-[425px]",
   drawerHeader: "text-left",
   drawerContent: "px-4",
@@ -49,41 +56,86 @@ function ProfileForm({
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+
   const [inputEffect, setInputEffect] = useState(false);
 
+  let flag = true;
+
+  const [accept, setAccept] = useState(false);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     if (!email || !name || !message) {
       toast.error(t("DialogForm.Errors.MissingFields"));
       setInputEffect(true);
       return;
     }
 
-    setLoading(true);
+    setAccept((prevAccept) => {
+      if (email === "" || !prevAccept || name === "" || message === "") {
+        flag = false;
+      } else {
+        setLoading(true);
+      }
+      return prevAccept;
+    });
 
-    try {
-      await emailjs.sendForm(
-        "service_vr92r5g",
-        "template_dvr1x78",
-        e.currentTarget,
-        "5J3vip7CH5ZH9OLZv"
-      );
-      toast.success(t("DialogForm.Success.EmailSent"));
-      closeDialog();
-    } catch (error) {
-      console.error("Error sending email:", error);
-      toast.error(t("DialogForm.Errors.EmailFailed"));
-    } finally {
-      setLoading(false);
+    if (flag) {
+      setLoading(true);
+      const form = document.createElement("form");
+
+      form.setAttribute("action", "");
+      form.setAttribute("method", "post");
+
+      const nameInput = document.createElement("input");
+      nameInput.setAttribute("type", "hidden");
+      nameInput.setAttribute("name", "name");
+      nameInput.setAttribute("value", name);
+      form.appendChild(nameInput);
+
+      const emailInput = document.createElement("input");
+      emailInput.setAttribute("type", "hidden");
+      emailInput.setAttribute("name", "email");
+      emailInput.setAttribute("value", email);
+      form.appendChild(emailInput);
+
+      const messageInput = document.createElement("input");
+      messageInput.setAttribute("type", "hidden");
+      messageInput.setAttribute("name", "message");
+      messageInput.setAttribute("value", message);
+      form.appendChild(messageInput);
+
+      document.body.appendChild(form);
+
+      try {
+        emailjs.sendForm(
+          "service_vr92r5g",
+          "template_dvr1x78",
+          form,
+          "5J3vip7CH5ZH9OLZv"
+        );
+        console.log("Email sent successfully");
+      } catch (error) {
+        console.error("Error sending email:", error);
+      } finally {
+        document.body.removeChild(form);
+        closeDialog();
+        toast(
+          <div className="flex items-center justify-start gap-1">
+            <span className="">
+              <HiOutlineCheckCircle />
+            </span>
+
+            <span>{t("About.Contact.MessageNote")}</span>
+          </div>
+        );
+      }
     }
   };
-
   const direction = i18n.language === "ar" ? "rtl" : "ltr";
 
   return (
     <form
-      className={cn(styles.form, "bg-[var(--background)]", className)}
+      className={cn(styles.form, "bg-[var(--background)]   ", className)}
       onSubmit={handleSubmit}
       dir={direction}
     >
@@ -102,6 +154,15 @@ function ProfileForm({
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        {email === "" && accept && (
+          <div className="text-red-300 flex justify-start items-center gap-1">
+            <span className="text-sm">
+              <HiOutlineExclamationCircle />
+            </span>
+
+            <span className=" text-sm">{t("DialogForm.Erros.Name")}</span>
+          </div>
+        )}
       </div>
 
       <div className={styles.inputGrid}>
@@ -110,7 +171,7 @@ function ProfileForm({
             "bg-[var(--input-background)] text-[var(--input-text)] placeholder-[var(--paragraph)] border-0",
             {
               "animate__shakeX animate__animated border-1 border border-red-400":
-                inputEffect && !email,
+                inputEffect && !name,
             }
           )}
           type="email"
@@ -119,6 +180,15 @@ function ProfileForm({
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+
+        {email === "" && accept && (
+          <div className="text-red-300 flex justify-start items-center gap-1">
+            <span className="text-sm">
+              <HiOutlineExclamationCircle />
+            </span>
+            <span className=" text-sm">{t("DialogForm.Erros.Email")}</span>
+          </div>
+        )}
       </div>
 
       <div className={styles.inputGrid}>
@@ -127,7 +197,7 @@ function ProfileForm({
             "bg-[var(--input-background)] text-[var(--input-text)] placeholder-[var(--paragraph)] border-0",
             {
               "animate__shakeX animate__animated border-1 border border-red-400":
-                inputEffect && !message,
+                inputEffect && !name,
             }
           )}
           id="message"
@@ -135,6 +205,15 @@ function ProfileForm({
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
+
+        {email === "" && accept && (
+          <div className="flex items-center justify-start gap-1 w-full text-red-300 ">
+            <span className="text-sm">
+              <HiOutlineExclamationCircle />
+            </span>
+            <span className=" text-sm">{t("DialogForm.Erros.Message")}</span>
+          </div>
+        )}
       </div>
 
       {!loading && (
@@ -176,7 +255,6 @@ export function DrawerDialogDemo() {
       </p>
     </div>
   );
-
   const closeDialog = () => {
     setOpen(false);
   };
@@ -194,6 +272,7 @@ export function DrawerDialogDemo() {
             <DialogTitle className="text-[var(--headline)] w-full text-center">
               {t("DialogForm.DialogTitle")}
             </DialogTitle>
+            <DialogDescription></DialogDescription>
           </DialogHeader>
           <ProfileForm closeDialog={closeDialog} />
         </DialogContent>
@@ -210,8 +289,10 @@ export function DrawerDialogDemo() {
         <DrawerContent className="bg-[var(--background)]  border-none p-[1rem]">
           <DrawerHeader className={styles.drawerHeader}>
             <DrawerTitle className="cursor-pointer"></DrawerTitle>
+            <DrawerDescription></DrawerDescription>
           </DrawerHeader>
           <ProfileForm closeDialog={closeDialog} />
+          <DrawerFooter className={styles.drawerFooter}></DrawerFooter>
         </DrawerContent>
       </Drawer>
     </div>
